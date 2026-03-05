3cad7a3f43a4f51c50dbdad67471de224be6b26a
Denis <denis.society@yandex.ru>
2026-03-05 20:49:49 +0500

    Лабораторная работа 2

---
+++ Program1.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+// Задание 1
+let readFloats count =
+    [ for index in 1 .. count do
+        printf "Введите вещественное число %d: " index
+        let input = Console.ReadLine()
+        yield float input]
+
+[<EntryPoint>]
+let main args =
+    printf "Введите количество значений списка: "
+    let count = Console.ReadLine() |> int
+
+    if count <= 0 then
+        printf "Ошибка: количество должно быть положительным!"
+        exit 1
+    else
+        let originalFloats = readFloats count
+        let integers = List.map int originalFloats
+
+        printfn "Список тип float: %A" originalFloats
+        printfn "Список тип integer: %A" integers
+    0
---
+++ Program2.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+let readStrings count =
+    [ for index in 1 .. count do
+        printf "Введите элемент %d: " index
+        yield Console.ReadLine() ]
+
+[<EntryPoint>]
+let main args =
+    printf "Введите количество элементов: "
+    let count = Console.ReadLine() |> int
+
+    if count <= 0 then
+        printfn "Ошибка: количество должно быть положительным!"
+        exit 1
+    else
+        let strings = readStrings count
+        
+        let evenCount = 
+            List.fold (fun (acc: int) (s: string) -> 
+                if s.Length % 2 = 0 then 
+                    acc + 1 
+                else 
+                    acc
+            ) 0 strings
+        
+        printfn "\nСписок строк: %A" strings
+        printfn "Строк с чётной длиной: %d" evenCount
+        0
